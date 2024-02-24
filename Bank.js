a6d4e4bd4526b3f9aa900cb78e796a358472b348
Sidra Gondal <150411149+geekycoder2@users.noreply.github.com>
2024-02-24 15:16:21 +0500

    Add files via upload

---
+++ Bank.js
@@ -0,0 +1,122 @@
+// import { faker } from '@faker-js/faker';
+// import chalk from 'chalk';
+// import inquirer from "inquirer";
+// // Customer Class
+// class Customer {
+//     firstName;
+//     lastName;
+//     age;
+//     gender;
+//     mobNumber;
+//     accNumber;
+//     constructor(fName, lName, gender, age, mob, acc) {
+//         this.firstName = fName;
+//         this.lastName = lName;
+//         this.age = age;
+//         this.gender = gender;
+//         this.mobNumber = mob;
+//         this.accNumber = acc;
+//     }
+// }
+// // Class Bank
+// class Bank {
+//     customer = [];
+//     account = [];
+//     addCustomer(obj) {
+//         this.customer.push(obj);
+//     }
+//     addAccountNumber(obj) {
+//         this.account.push(obj);
+//     }
+//     transaction(accObj) {
+//         let NewAccounts = this.account.filter((acc) => acc.accNumber !== accObj.accNumber);
+//         this.account = [...NewAccounts, accObj];
+//     }
+// }
+// let meezanBank = new Bank();
+// // Custumer Create
+// for (let i = 1; i <= 3; i++) {
+//     let fName = faker.person.firstName("male");
+//     let lName = faker.person.lastName();
+//     let num = parseInt(faker.phone.number("3#########"));
+//     const cus = new Customer(fName, lName, "male", 20 * i, num, 1000 + i);
+//     meezanBank.addCustomer(cus);
+//     meezanBank.addAccountNumber({ accNumber: cus.accNumber, balance: 1000 * i });
+// }
+// // Bank Functionality
+// async function bankService(bank) {
+//     do {
+//         let service = await inquirer.prompt({
+//             type: "list",
+//             name: "select",
+//             message: "Please Select the Service",
+//             choices: ["View Balance", "Cash Withdraw", "Cash Deposite", "Exit"]
+//         });
+//         // View Balance
+//         if (service.select == "View Balance") {
+//             let res = await inquirer.prompt({
+//                 type: "input",
+//                 name: "num",
+//                 message: "Please Enter Your Account Number:",
+//             });
+//             let account = meezanBank.account.find((acc) => acc.accNumber == res.num);
+//             if (!account) {
+//                 console.log(chalk.red.bold.italic("Invalid Account Number"));
+//             }
+//             if (account) {
+//                 let name = meezanBank.customer.find((item) => item.accNumber == account?.accNumber);
+//                 console.log(`Dear ${chalk.green.italic(name?.firstName)} ${chalk.green.italic(name?.lastName)} Your Account Balance is ${chalk.bold.blueBright("$", account.balance)}`);
+//             }
+//         }
+//         // Cash Withdrw
+//         if (service.select == "Cash Withdraw") {
+//             let res = await inquirer.prompt({
+//                 type: "input",
+//                 name: "num",
+//                 message: "Please Enter Your Account Number:",
+//             });
+//             let account = meezanBank.account.find((acc) => acc.accNumber == res.num);
+//             if (!account) {
+//                 console.log(chalk.red.bold.italic("Invalid Account Number"));
+//             }
+//             if (account) {
+//                 let ans = await inquirer.prompt({
+//                     type: "number",
+//                     message: "Please Enter Your amount.",
+//                     name: "rupee",
+//                 });
+//                 let newBalance = account.balance - ans.rupee;
+//                 // transaction method call
+//                 bank.transaction({ accNumber: account.accNumber, balance: newBalance });
+//             }
+//         }
+//         // Cash Deposite
+//         if (service.select == "Cash Deposite") {
+//             let res = await inquirer.prompt({
+//                 type: "input",
+//                 name: "num",
+//                 message: "Please Enter Your Account Number:",
+//             });
+//             let account = meezanBank.account.find((acc) => acc.accNumber == res.num);
+//             if (!account) {
+//                 console.log(chalk.red.bold.italic("Invalid Account Number"));
+//             }
+//             if (account) {
+//                 let ans = await inquirer.prompt({
+//                     type: "number",
+//                     message: "Please Enter Your amount.",
+//                     name: "rupee",
+//                 });
+//                 if (ans.rupee > account.balance) {
+//                     console.log(chalk.red.bold("Insuficiant Balance...."));
+//                     let newBalance = account.balance + ans.rupee;
+//                     // transaction method call
+//                     bank.transaction({ accNumber: account.accNumber, balance: newBalance });
+//                 }
+//             }
+//         }
+//         if (service.select == "Exit")
+//             ;
+//     } while (true);
+// }
+// bankService(meezanBank);
